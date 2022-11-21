427dba4e5ef0e219fda3de5b2ccefeec8fc2b69a
Kacper Chołody <cholody.kacper1998@gmail.com>
2022-11-21 18:20:52 +0100

    prevent multiple clicks on refresh button

--- app/src/main/java/com/example/servercommander/fragments/HomeFragment.kt
+++ app/src/main/java/com/example/servercommander/fragments/HomeFragment.kt
@@ -67,61 +67,77 @@ class HomeFragment : Fragment() {
         val heaviestApp = binding.HeaviestProcessInfo.heaviestProcessValue
 
         refreshWidget.setOnClickListener {
-            if(sharedPref.contains(getString(R.string.server_url)) and
-                sharedPref.contains(getString(R.string.username)) and
-                sharedPref.contains(getString(R.string.pubkey)) and
-                sharedPref.contains(getString(R.string.connectionTested))) {
+            if(refreshWidget.isClickable)
+            {
+                refreshWidget.isClickable = false
 
-                sshConnection = SshConnection(
-                    sharedPref.getString(getString(R.string.server_url), "").toString(),
-                    22,
-                    sharedPref.getString(getString(R.string.username), "").toString(),
-                    sharedPref.getString(getString(R.string.pubkey), "").toString()
-                )
+                if(sharedPref.contains(getString(R.string.server_url)) and
+                    sharedPref.contains(getString(R.string.username)) and
+                    sharedPref.contains(getString(R.string.pubkey)) and
+                    sharedPref.contains(getString(R.string.connectionTested))) {
 
-                if ( sharedPref.getBoolean(getString(R.string.connectionTested), false) ){
+                    sshConnection = SshConnection(
+                        sharedPref.getString(getString(R.string.server_url), "").toString(),
+                        22,
+                        sharedPref.getString(getString(R.string.username), "").toString(),
+                        sharedPref.getString(getString(R.string.pubkey), "").toString()
+                    )
 
-                    val coroutineScope = MainScope()
-                    coroutineScope.launch {
-                        val defer = async(Dispatchers.IO) {
-                            sshConnection.executeRemoteCommandOneCall("python3 ~/copilot/main.py --dash")
+                    if ( sharedPref.getBoolean(getString(R.string.connectionTested), false) ){
+
+                        var rotation: Boolean = true
+
+                        val coroutineScope = MainScope()
+                        coroutineScope.launch {
+                            val defer = async(Dispatchers.IO) {
+                                sshConnection.executeRemoteCommandOneCall("python3 ~/copilot/main.py --dash")
+                            }
+
+                            val output = defer.await()
+
+                            rotation = false
+
+                            val jsonObject = JSONTokener(output).nextValue() as JSONObject
+
+                            tempText.text = jsonObject.getString("cpu_temp")
+                            cpuUsage.text = jsonObject.getString("cpu_usage")
+                            ramUsage.text = jsonObject.getString("ram_usage")
+
+                            linuxKernelVersion.text = jsonObject.getString("kernel")
+                            hostname.text = jsonObject.getString("hostname")
+
+                            uptime.text = jsonObject.getString("uptime")
+                            localIpAddress.text = jsonObject.getString("local_ip")
+                            publicIpAddress.text = jsonObject.getString("public_ip")
+                            diskUsage.text = jsonObject.getString("disk_usage")
+                            diskName.text = jsonObject.getString("disk_name")
+                            heaviestApp.text = jsonObject.getString("stress_app")
+
                         }
-
-                        refreshWidget.animate().apply {
-                            duration = 1000
-                            rotationBy(360f)
-                        }.withEndAction{
-                            Toast.makeText(context, "Refreshed manually", Toast.LENGTH_SHORT).show()
-                        }.start()
-
-                        val output = defer.await()
-
-                        val jsonObject = JSONTokener(output).nextValue() as JSONObject
-
-                        tempText.text = jsonObject.getString("cpu_temp")
-                        cpuUsage.text = jsonObject.getString("cpu_usage")
-                        ramUsage.text = jsonObject.getString("ram_usage")
-
-                        linuxKernelVersion.text = jsonObject.getString("kernel")
-                        hostname.text = jsonObject.getString("hostname")
-
-                        uptime.text = jsonObject.getString("uptime")
-                        localIpAddress.text = jsonObject.getString("local_ip")
-                        publicIpAddress.text = jsonObject.getString("public_ip")
-                        diskUsage.text = jsonObject.getString("disk_usage")
-                        diskName.text = jsonObject.getString("disk_name")
-                        heaviestApp.text = jsonObject.getString("stress_app")
-
+                        fun rotate(){
+                            refreshWidget.animate().apply {
+                                duration = 1000
+                                rotationBy(360f)
+                            }.withEndAction{
+                                if (rotation) rotate()
+                                else
+                                {
+                                    refreshWidget.isClickable = true
+                                    Toast.makeText(context, "Refreshed manually", Toast.LENGTH_SHORT).show()
+                                }
+                            }.start()
+                        }
+                        rotate()
+                    }
+                    else
+                    {
+                        Toast.makeText(context, "You need to test your connection first. Please click red server icon at the top right corner", Toast.LENGTH_LONG).show()
                     }
                 }
                 else
                 {
-                    Toast.makeText(context, "You need to test your connection first. Please click red server icon at the top right corner", Toast.LENGTH_LONG).show()
+                    Toast.makeText(context, "Connection to server is not possible with given settings", Toast.LENGTH_SHORT).show()
                 }
-            }
-            else
-            {
-                Toast.makeText(context, "Connection to server is not possible with given settings", Toast.LENGTH_SHORT).show()
             }
         }
 
@@ -183,7 +199,7 @@ class HomeFragment : Fragment() {
 
                     fun rotate(){
                         connectionTest.animate().apply {
-                            duration = 500
+                            duration = 1000
                             rotationBy(360f)
                         }.withEndAction{
                             if (rotation)
