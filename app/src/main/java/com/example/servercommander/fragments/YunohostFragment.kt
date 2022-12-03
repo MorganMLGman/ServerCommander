c6e03a12cb498feddab84e49e8ff5ea2edfd88ae
Kacper Chołody <cholody.kacper1998@gmail.com>
2022-12-03 16:42:13 +0100

    YunohostFragment.kt preparation

---
+++ app/src/main/java/com/example/servercommander/fragments/YunohostFragment.kt
@@ -0,0 +1,42 @@
+package com.example.servercommander.fragments
+
+import android.content.Context
+import android.content.SharedPreferences
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.servercommander.R
+import com.example.servercommander.SshConnection
+import com.example.servercommander.databinding.FragmentYunohostBinding
+
+class YunohostFragment : Fragment() {
+    private var _binding: FragmentYunohostBinding? = null
+    private val binding get() = _binding!!
+
+    private lateinit var sharedPref: SharedPreferences
+    private lateinit var sshConnection: SshConnection
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        sharedPref = requireActivity().getSharedPreferences(
+            getString(R.string.app_name), Context.MODE_PRIVATE
+        )
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentYunohostBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+    }
+}
