2e5f4cc0c873dccfbb445a7d1dded8814c9be9b1
cichy1173 <cichy1173@gmail.com>
2022-12-03 18:29:27 +0100

    some yunohost stuff

--- app/src/main/java/com/example/servercommander/fragments/YunohostFragment.kt
+++ app/src/main/java/com/example/servercommander/fragments/YunohostFragment.kt
@@ -1,11 +1,15 @@
 package com.example.servercommander.fragments
 
 import android.content.Context
+import android.content.Intent
 import android.content.SharedPreferences
+import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.WebView
 import androidx.fragment.app.Fragment
 import com.example.servercommander.R
 import com.example.servercommander.SshConnection
@@ -32,11 +36,34 @@ class YunohostFragment : Fragment() {
     ): View {
         _binding = FragmentYunohostBinding.inflate(inflater, container, false)
         return binding.root
+
+
+
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        val openSSOButton = binding.openSSOButton
+        val goToAdminPage = binding.moreButton
+
+        val ssoWebpage = "https://" + sharedPref.getString(getString(R.string.server_url), "").toString() + "/yunohost/sso/portal.html"
+        val adminWebPage = "https://" + sharedPref.getString(getString(R.string.server_url), "").toString() + "/yunohost/admin/"
+
+        openSSOButton.setOnClickListener{
+            var intent = Intent(Intent.ACTION_VIEW, Uri.parse(ssoWebpage))
+            startActivity(intent)
+        }
+
+        goToAdminPage.setOnClickListener{
+            var intent = Intent(Intent.ACTION_VIEW, Uri.parse(adminWebPage))
+            startActivity(intent)
+        }
+
+
+
+
 
     }
+
 }
